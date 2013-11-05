134ef9b5c403e9d1e0b8e0489dd00306b99028e2
Bruno Rocha <rochacbruno@gmail.com>
2013-11-05 10:47:59 -0200

    processot notification and confirmation handlers

--- processors/pagseguro_processor.py
+++ processors/pagseguro_processor.py
@@ -63,18 +63,95 @@ class PagSeguroProcessor(BaseProcessor):
         else:
             self.pg.notification_url = self.config.get('notification_url')
 
-        logger.debug("Processor validated {}".format(self.__dict__))
+        self.cart.addlog("pagSeguro validated {}".format(self.pg.data))
         return True  # all data is valid
 
     def process(self, *args, **kwargs):
-        response = self.pg.checkout()
+        kwargs.update(self._record.config)
+        kwargs.update(self.cart.config)
+        response = self.pg.checkout(**kwargs)
         if not response.errors:
             self.cart.checkout_code = response.code
-            self.cart.status = 'checked_out'
-            self.cart.save()
-            logger.debug("Processed! {}".format(response.code))
+            #self.cart.status = 'checked_out'  # should set on redirect url
+            self.cart.addlog("PagSeguro processed! {}".format(response.code))
             return redirect(response.payment_url)
         else:
-            logger.debug('Error processing {}'.format(response.errors))
+            self.cart.addlog(
+                'PagSeguro error processing {}'.format(
+                    response.errors
+                )
+            )
             return render_template("cart/checkout_error.html",
-                                   response=response)
+                                   response=response, cart=self.cart)
+
+    def notification(self):
+        code = request.form.get('notificationCode')
+        if not code:
+            return "notification code not found"
+
+        response = self.pg.check_notification(code)
+        reference = getattr(response, 'reference', None)
+        if not reference:
+            return "reference not found"
+
+        PREFIX = self.pg.config.get('REFERENCE_PREFIX', '') or ''
+        PREFIX = PREFIX.replace('%s', '')
+
+        status = getattr(response, 'status', None)
+        transaction_code = getattr(response, 'code', None)
+
+        # TODO: get grossAmount to populate a payment with methods
+        try:
+            self.cart = Cart.objects.get(
+                reference_code=reference.replace(PREFIX, '')
+            )
+            self.cart.status = self.STATUS_MAP.get(
+                str(status), self.cart.status
+            )
+            if transaction_code:
+                self.cart.transaction_code = transaction_code
+            msg = "Status changed to: %s" % self.cart.status
+            self.cart.addlog(msg)
+            return msg
+        except Exception as e:
+            msg = "Cart not found: {} - {}".format(reference, e)
+            logger.error(msg)
+            return msg
+
+    def confirmation(self):  # redirect_url
+        context = {}
+        transaction_param = self.config.get(
+            'transaction_param',
+            self.pg.config.get('TRANSACTION_PARAM', 'transaction_id')
+        )
+        transaction_code = request.args.get(transaction_param)
+        if transaction_code:
+            context['transaction_code'] = transaction_code
+            response = self.pg.check_transaction(transaction_code)
+            reference = getattr(response, 'reference', None)
+            if not reference:
+                return render_template('cart/simple_confirmation.html',
+                                       **context)
+            PREFIX = self.pg.config.get('REFERENCE_PREFIX', '') or ''
+            PREFIX = PREFIX.replace('%s', '')
+
+            status = getattr(response, 'status', None)
+
+            # TODO: get grossAmount to populate a payment with methods
+            try:
+                self.cart = Cart.objects.get(
+                    reference_code=reference.replace(PREFIX, '')
+                )
+                self.cart.status = self.STATUS_MAP.get(
+                    str(status), self.cart.status
+                )
+                if not self.cart.transaction_code:
+                    self.cart.transaction_code = transaction_code
+                msg = "Status changed to: %s" % self.cart.status
+                self.cart.addlog(msg)
+                context['cart'] = self.cart
+                return render_template('cart/confirmation.html', **context)
+            except Exception as e:
+                msg = "Cart not found: {} - {}".format(reference, e)
+                logger.error(msg)
+        return render_template('cart/simple_confirmation.html', **context)
